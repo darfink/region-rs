b3ae8ef4fc8af8f2a9d805d14140434ac5b600b5
Elliott Linder <elliott.darfink@gmail.com>
2017-06-18 09:50:33 +0200

    refactor: explicitly export os functions

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -1,26 +1,37 @@
-#[cfg(unix)]
-mod unix;
-
-#[cfg(unix)]
-pub use self::unix::*;
-
 #[cfg(windows)]
 mod windows;
 
 #[cfg(windows)]
-pub use self::windows::*;
+pub use self::windows::{
+    page_size,
+    get_region,
+    set_protection,
+    unlock,
+    lock,
+};
+
+#[cfg(unix)]
+mod unix;
+
+#[cfg(unix)]
+pub use self::unix::{
+    page_size,
+    set_protection,
+    unlock,
+    lock,
+};
 
 #[cfg(target_os = "macos")]
 mod macos;
 
 #[cfg(target_os = "macos")]
-pub use self::macos::*;
+pub use self::macos::get_region;
 
 #[cfg(target_os = "linux")]
 mod linux;
 
 #[cfg(target_os = "linux")]
-pub use self::linux::*;
+pub use self::linux::get_region;
 
 pub fn page_floor(address: usize) -> usize {
     address & !(page_size() - 1)
