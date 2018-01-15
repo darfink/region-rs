ee5ed2aa64135c5a75178805bcfe90fe82dd5d7b
Elliott Linder <elliott.darfink@gmail.com>
2018-01-15 12:45:47 +0100

    style(error): some reformatting...

--- src/error.rs
+++ src/error.rs
@@ -5,6 +5,7 @@ use failure;
 /// The result type used by this library.
 pub type Result<T> = ::std::result::Result<T, failure::Error>;
 
+/// A collection of possible errors.
 #[derive(Debug, Fail)]
 pub enum Error {
     /// The supplied address is null.
@@ -16,13 +17,13 @@ pub enum Error {
     /// Invalid procfs input.
     #[fail(display = "invalid procfs source input")]
     ProcfsInput,
-    /// A system call failed.
-    #[fail(display = "system call failed with: {}", _0)]
-    SystemCall(::errno::Errno),
-    /// macOS kernel call failed
-    #[fail(display = "macOS kernel call failed with: {}", _0)]
-    MachRegion(::libc::c_int),
     /// A proc fs IO operation failed.
     #[fail(display = "{}", _0)]
     ProcfsIo(#[cause] ::std::io::Error),
+    /// A system call failed.
+    #[fail(display = "system call failed with: {}", _0)]
+    SystemCall(::errno::Errno),
+    /// A macOS kernel call failed
+    #[fail(display = "macOS kernel call failed with: {}", _0)]
+    MachRegion(::libc::c_int),
 }
