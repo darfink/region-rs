06f226bb064626b6ef0eff681a43a545c569aabc
Elliott Linder <elliott.darfink@gmail.com>
2018-05-04 19:49:10 +0200

    fix: add conversion from 'io::Error'

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 //! Error types and utilities.
 
+use std::io;
+
 /// The result type used by this library.
 pub type Result<T> = ::std::result::Result<T, Error>;
 
@@ -17,11 +19,17 @@ pub enum Error {
   ProcfsInput,
   /// A proc fs IO operation failed.
   #[fail(display = "{}", _0)]
-  ProcfsIo(#[cause] ::std::io::Error),
+  ProcfsIo(#[cause] io::Error),
   /// A system call failed.
   #[fail(display = "system call failed with: {}", _0)]
   SystemCall(::errno::Errno),
   /// A macOS kernel call failed
   #[fail(display = "macOS kernel call failed with: {}", _0)]
   MachRegion(::libc::c_int),
+}
+
+impl From<io::Error> for Error {
+  fn from(error: io::Error) -> Self {
+    Error::ProcfsIo(error)
+  }
 }
