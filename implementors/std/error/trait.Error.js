660c937b0aca31b4694d8ccfc595ff7ee066f103
darfink <darfink@users.noreply.github.com>
2021-05-18 21:30:30 +0000

    deploy: 5d009d90c1798506a1266adb0e4ceb8cd5f3d096

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["region"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"region/enum.Error.html\" title=\"enum region::Error\">Error</a>","synthetic":false,"types":["region::error::Error"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["region"] = {"name":"","dirs":[{"name":"os","files":["linux.rs","mod.rs","unix.rs"]}],"files":["error.rs","lib.rs","lock.rs","page.rs","protect.rs","query.rs","util.rs"]};
+createSourceSidebar();
